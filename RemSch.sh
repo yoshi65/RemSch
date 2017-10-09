52f02393dd3a45db9674d5a63c515edaa61338f5
Yamazaki Yoshitaka <yamazaki@fun.bio.keio.ac.jp>
2017-10-09 17:51:40 +0900

    updated execution speed

--- RemSch.sh
+++ RemSch.sh
@@ -6,14 +6,15 @@ local -a del
 num=0
 del_num=0
 time=`date +"%k"`
-today_line=`/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1 | grep -n "today" | sed -e 's/:.*//g'`
-tomorrow_line=`/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1 | grep -n "tomorrow" | sed -e 's/:.*//g'`
-today_sche_list=(`/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1 | head -n $(($tomorrow_line-1)) | grep -n "\-.[0-9][0-9]:[0-9][0-9]" | awk '{print $1} {print $4}' | sed -e 's/:.*//g'`)
+original=`/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1`
+today_line=`echo $original | grep -n "today" | sed -e 's/:.*//g'`
+tomorrow_line=`echo $original | grep -n "tomorrow" | sed -e 's/:.*//g'`
+today_sche_list=(`echo $original | head -n $(($tomorrow_line-1)) | grep -n "\-.[0-9][0-9]:[0-9][0-9]" | awk '{print $1} {print $4}' | sed -e 's/:.*//g'`)
 
 # keep full time of today's schedule
 for i in `seq $(($today_line+2)) $(($tomorrow_line-1))`
 do
-	if [ `	/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1 | awk "$i <= NR && NR <= $(($i+1))" | grep -c "•"` = 2 ] ; then
+	if [ `echo $original | awk "$i <= NR && NR <= $(($i+1))" | grep -c "•"` = 2 ] ; then
 		today_line=$i
 	fi
 done
@@ -27,9 +28,9 @@ done
 
 # delete ended schedule
 if test $num -eq 0; then
-	data=`/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1`
+	data=$original
 else
-	data=`/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1 | awk "NR <= $(($today_line+1)) || $num <= NR"`
+	data=`echo $original | awk "NR <= $(($today_line+1)) || $num <= NR"`
 fi
 
 # delete extra notes

2b27f2d7f3ee0ee763d3ebe9b9689633b94e600b
Yamazaki Yoshitaka <yamazaki@fun.bio.keio.ac.jp>
2017-10-11 15:54:01 +0900

    updated comparison of time

--- RemSch.sh
+++ RemSch.sh
@@ -5,11 +5,11 @@ local -A today_sche_list
 local -a del
 num=0
 del_num=0
-time=`date +"%k"`
+time=`date +"%k%M"`
 original=`/usr/local/bin/icalBuddy -f -sd -nc eventsToday+1`
 today_line=`echo $original | grep -n "today" | sed -e 's/:.*//g'`
 tomorrow_line=`echo $original | grep -n "tomorrow" | sed -e 's/:.*//g'`
-today_sche_list=(`echo $original | head -n $(($tomorrow_line-1)) | grep -n "\-.[0-9][0-9]:[0-9][0-9]" | awk '{print $1} {print $4}' | sed -e 's/:.*//g'`)
+today_sche_list=(`echo $original | head -n $(($tomorrow_line-1)) | grep -n "\-.[0-9][0-9]:[0-9][0-9]" | awk '{print $1} {print $4}' | sed -e 's/:\([0-9][0-9]\).*/\1/g;s/:.*//g'`)
 
 # keep full time of today's schedule
 for i in `seq $(($today_line+2)) $(($tomorrow_line-1))`

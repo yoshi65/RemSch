1ce200b10853b6dd43680263b9c85047b359f1a3
Yamazaki Yoshitaka <yamazaki@fun.bio.keio.ac.jp>
2018-06-18 11:21:30 +0900

    fixed bug

--- RemSch.sh
+++ RemSch.sh
@@ -56,7 +56,7 @@ done
 if test $num -eq 0; then
 	data=$original
 else
-	data=`echo $original | awk "NR <= $((${today_line}+1)) || $num < NR"`
+	data=`echo $original | awk "NR <= ${today_line} || $num < NR"`
 fi
 
 # delete extra contents
